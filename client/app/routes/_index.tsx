a5ed22b20d1f1b649b16059a4715d938a800b288
Rishi Viswanathan <128200692+risv1@users.noreply.github.com>
2023-11-14 22:05:50 +0530

    added carousel layout

--- client/app/routes/_index.tsx
+++ client/app/routes/_index.tsx
@@ -1,6 +1,7 @@
 import { LinksFunction } from "@remix-run/node";
 import { FC } from "react";
 import Navbar from "~/components/Navbar";
+import Carousel from "~/components/Carousel";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: "../styles/main.css" }];
@@ -10,6 +11,7 @@ const HomePage: FC = () => {
   return (
     <>
       <Navbar />
+      <Carousel />
       <div className="">
         <h1 className="text-5xl">Hello</h1>
       </div>
