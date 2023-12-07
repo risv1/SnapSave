ce7c8c736faa408fc5ebbc79ef8f788ddc2384c7
Rishi Viswanathan <128200692+risv1@users.noreply.github.com>
2023-12-07 22:37:22 +0530

    Delete routes directory

--- routes/admin.add-sport.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const AddSport = () => {
-    return(
-        <h1 className="text-3xl">Add Sport</h1>
-    )
-}
-
-export default AddSport
--- routes/admin.users._index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const UsersPage = () => {
-    return(
-        <h1 className="text-3xl">Users Page</h1>
-    )
-}
-
-export default UsersPage
